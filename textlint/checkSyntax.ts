33eea4880c60dd7c7b6a7ef735de04397397fe11
kei-s16 <kei1216s@nifty.com>
2023-07-07 23:33:46 +0900

    import textlint different way

--- textlint/checkSyntax.ts
+++ textlint/checkSyntax.ts
@@ -1,4 +1,4 @@
-import { createLinter, loadTextlintrc } from "npm:textlint@13.3.3";
+import { createLinter, loadTextlintrc } from "textlint";
 import { TextlintResult } from "npm:@textlint/kernel@13.3.3";
 import _1 from "npm:textlint-rule-preset-ja-technical-writing@7.0.0";
 import _2 from "npm:textlint-rule-preset-jtf-style@2.3.13";
